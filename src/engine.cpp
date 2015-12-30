2c764c8ddf3be13b79cfa46c7b81eeaf7f10a5b9
Colin Douch <colin@quirl.co.nz>
2015-12-30 22:09:53 +1300

    Added pause so we don't thrash the CPU

--- src/engine.cpp
+++ src/engine.cpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include "event.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 quarrel::engine::engine(std::string window_title, uint window_width, uint window_height) :
   game_window(new native_window(window_width, window_height))
@@ -13,6 +15,7 @@ quarrel::engine::engine(std::string window_title, uint window_width, uint window
 
 quarrel::engine::~engine(){
   delete game_window;
+  game_window = NULL;
 }
 
 bool quarrel::engine::is_running() const{
@@ -23,8 +26,12 @@ void quarrel::engine::event_loop(void){
   std::vector<std::shared_ptr<quarrel::event>> events = game_window->get_eventhandler()->get_all_events();
   for(std::shared_ptr<quarrel::event> event : events){
     if(event->type == WindowClosedEvent){
-      std::cout << "Closed!\n";
       game_window->close();
     }
   }
+
+  static int x = 0;
+  x += 1;
+  game_window->get_graphics()->fill_rectangle(x, 100, 50, 100);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
