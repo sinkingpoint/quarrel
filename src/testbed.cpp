1467fad86cb6935044df6f91d0ef4f769b025af6
Colin Douch <colin@quirl.co.nz>
2016-01-25 06:15:07 +0000

    Updated testbed to be better

--- src/testbed.cpp
+++ src/testbed.cpp
@@ -10,9 +10,22 @@ int main(int argc, char** args){
   std::shared_ptr<quarrel::entity> ent = std::shared_ptr<quarrel::entity>(new quarrel::ellipse_entity(50, 50, 50, 50, quarrel::color::green));
   scr.add_entity(ent);
   quarrel::engine engine("Quarrel Testbed", 640, 480, scr);
+  const quarrel::keyboard& keys = engine.get_keyboard();
 
   while(engine.is_running()){
-    ent->move(1, 0);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); //Return control back to the OS for stability
+    if(keys.is_key_down(113)){
+      ent->move(-1, 0);
+    }
+    else if(keys.is_key_down(114)){
+      ent->move(1, 0);
+    }
+
+    if(keys.is_key_down(111)){
+      ent->move(0, -1);
+    }
+    else if(keys.is_key_down(116)){
+      ent->move(0, 1);
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1)); //Return control back to the OS for stability
   }
 }
