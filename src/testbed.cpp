f28f7b5297fbce0671e1bc6946b2c1db258f38df
Colin Douch <colin@quirl.co.nz>
2016-01-25 04:48:59 +0000

    Updated testbed to show new multi threading

--- src/testbed.cpp
+++ src/testbed.cpp
@@ -1,18 +1,20 @@
 #include "engine.h"
 #include <memory>
 #include <X11/Xlib.h>
+#include <iostream>
 
 int main(int argc, char** args){
   UNUSED(argc);
   UNUSED(args);
 
   XInitThreads();
-
   quarrel::screen scr(quarrel::color::black);
   std::shared_ptr<quarrel::entity> ent = std::shared_ptr<quarrel::entity>(new quarrel::ellipse_entity(50, 50, 50, 50, quarrel::color::green));
-
   scr.add_entity(ent);
-
   quarrel::engine engine("Quarrel Testbed", 640, 480, scr);
-  while(engine.is_running()){}
+
+  while(engine.is_running()){
+    ent->move(1, 0);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); //Return control back to the OS for stability
+  }
 }
